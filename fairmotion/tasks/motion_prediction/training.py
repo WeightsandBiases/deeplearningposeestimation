cfa1468d51b9fb33082770876697a74a65eb96d7
Mike Henderson <mhenderson43@gatech.edu>
2021-04-20 15:57:23 -0700

    Added loss functions and plot MAE form mean pose

--- fairmotion/tasks/motion_prediction/training.py
+++ fairmotion/tasks/motion_prediction/training.py
@@ -8,6 +8,8 @@ import os
 import random
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import pickle
 
 from fairmotion.tasks.motion_prediction import generate, utils, test
 from fairmotion.utils import utils as fairmotion_utils
@@ -18,6 +20,56 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
     level=logging.INFO,
 )
+
+def get_train_stats():
+    pkl_file = open("stats_train.pkl", "rb")
+    angle_mean = pickle.load(pkl_file)
+    angle_stddev = pickle.load(pkl_file)
+    angle_percentile = pickle.load(pkl_file)
+    velocity_mean = pickle.load(pkl_file)
+    velocity_stddev = pickle.load(pkl_file)
+    velocity_percentile = pickle.load(pkl_file)
+    acceleration_mean = pickle.load(pkl_file)
+    acceleration_stddev = pickle.load(pkl_file)
+    acceleration_percentile = pickle.load(pkl_file)
+    pkl_file.close()
+
+    return (angle_mean, angle_stddev, angle_percentile,
+            velocity_mean, velocity_stddev, velocity_percentile,
+            acceleration_mean, acceleration_stddev, acceleration_percentile)
+
+class MSEWithDeviationLoss(nn.Module):
+    def __init__(self, factor=1):
+        super(MSEWithDeviationLoss, self).__init__()
+        stats = get_train_stats()
+        self.mean_pose = torch.Tensor(stats[0])
+        self.std_pose = torch.Tensor(stats[1] + 1e-8)
+        self.factor = factor
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        mean_tiled = self.mean_pose.expand(input.shape).type(input.dtype).to(input.device)
+        std_tiled = self.std_pose.expand(input.shape).type(input.dtype).to(input.device)
+
+        normalization = torch.abs((input - mean_tiled)/std_tiled*self.factor).mean()
+        return F.mse_loss(input, target) + normalization
+
+
+
+class MSEWithOutlierLoss(nn.Module):
+    def __init__(self, factor=1):
+        super(MSEWithOutlierLoss, self).__init__()
+        stats = get_train_stats()
+        self.max_ranges = torch.Tensor(stats[2][:, 99].reshape(1, stats[2].shape[0]))
+        self.min_ranges = torch.Tensor(stats[2][:, 1].reshape(1, stats[2].shape[0]))
+        self.factor = factor
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        max_tiled = self.max_ranges.expand(input.shape).type(input.dtype).to(input.device)
+        min_tiled = self.min_ranges.expand(input.shape).type(input.dtype).to(input.device)
+
+        upper_error = (((torch.where(max_tiled > input, max_tiled, input) - max_tiled) * self.factor) ** 2).mean()
+        lower_error = (((torch.where(min_tiled < input, min_tiled, input) - min_tiled) * self.factor) ** 2).mean()
+        return F.mse_loss(input, target) + upper_error + lower_error
 
 
 def set_seeds():
@@ -32,6 +84,10 @@ def select_criterion(args):
         criterion = nn.L1Loss()
     elif args.criterion == "sl1":
         criterion = nn.SmoothL1Loss()
+    elif args.criterion == "d":
+        criterion = MSEWithDeviationLoss()
+    elif args.criterion == "o":
+        criterion = MSEWithOutlierLoss()
     else:
         criterion = nn.MSELoss()
     return criterion
@@ -132,7 +188,7 @@ def train(args):
         )
         if epoch % args.save_model_frequency == 0:
             _, rep = os.path.split(args.preprocessed_path.strip("/"))
-            _, mae = test.test_model(
+            _, mae, _ = test.test_model(
                 model=model,
                 dataset=dataset["validation"],
                 rep=rep,
@@ -227,7 +283,7 @@ if __name__ == "__main__":
             "tied_seq2seq",
             "transformer",
             "transformer_encoder",
-            "rnn",
+            "rnn"
         ],
     )
     parser.add_argument(
@@ -245,7 +301,7 @@ if __name__ == "__main__":
         type=str,
         help="Loss Function",
         default="mse",
-        choices=["mse", "l1", "sl1"],
+        choices=["mse", "l1", "sl1", "d", "o"],
     )
     args = parser.parse_args()
     main(args)
