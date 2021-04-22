e150518e94db530524aaecfa0c589cc48ecf2dc0
David Doellstedt <ddoellstedt3@gatech.edu>
2021-04-21 19:52:03 -0700

    fix

--- fairmotion/tasks/motion_prediction/training.py
+++ fairmotion/tasks/motion_prediction/training.py
@@ -42,8 +42,8 @@ def get_derivative(input):
     return input[:, 1:, :] - input[:, :-1, :]
 
 def zero_out_threshold(input, threshold):
-    input = torch.where(input < -threshold, input, 0)
-    input = torch.where(input > threshold, input, 0)
+    input = torch.where(input < -threshold, 0, input)
+    input = torch.where(input > threshold, 0, input)
     return input
 
 class MSEWithDeviationLoss(nn.Module):
